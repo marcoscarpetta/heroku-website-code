8a5a4f91eb7668493e0cfe9cb9a788426ef47271
Marco Scarpetta <marcoscarpetta02@gmail.com>
2018-01-12 17:24:30 +0100

    Update copyright year

--- context_processor.py
+++ context_processor.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 Marco Scarpetta
+# Copyright (C) 2017-2018 Marco Scarpetta
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
